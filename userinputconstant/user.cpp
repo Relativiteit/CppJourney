ed15c9e8a631d3f367e3a1ff43243da8ba203783
Relativiteit <alejocain@gmail.com>
2023-03-13 22:59:41 +0100

    userInput

---
+++ userinputconstant/user.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+   int x, y = 3;
+   string nameOfUser = "Alejo";
+   cout << nameOfUser;
+}
+
+// basic comments in c++ 
+/* multi line comments */
