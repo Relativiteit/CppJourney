700a3c6e5b1a53c21ffbf26fa78921b949b6d446
Relativiteit <alejocain@gmail.com>
2023-03-14 08:57:26 +0100

    arrays

---
+++ arrays/array.cpp
@@ -0,0 +1,18 @@
+// arrays are different in C++ 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int x = 5;
+    int arr[] = {4,5,6,7};
+    string arr2[]= {"henk", "alejo", "good"};
+    float arr3[] = {1.1, 2.2, 3.3, 4,5};
+
+    cout << sizeof(arr2)/sizeof(arr2[0]);
+
+    
+
+
+
+}
